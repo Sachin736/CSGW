ddfb390dddb83600e97383b7a942d4eb564b4057
Sachin Gowda <sachings888@gmail.com>
2022-01-24 19:29:55 +0530

    edited, lambda created and onum file created

--- enum.py
+++ enum.py
@@ -27,9 +27,19 @@ while(num < len(list2)):
 # Python program to print even Numbers in a List
 
 # list of numbers
-list1 = [10, 21, 4, 45, 66, 93]
+list3 = [10, 21, 4, 45, 66, 93]
 
 # using list comprehension
-even_nos = [num for num in list1 if num % 2 == 0]
+even_nos = [num for num in list3 if num % 2 == 0]
+
+print("Even numbers in the list: ", even_nos)
+# Python program to print Even Numbers in a List
+
+# list of numbers
+list1 = [10, 21, 4, 45, 66, 93, 11]
+
+
+# we can also print even no's using lambda exp.
+even_nos = list(filter(lambda x: (x % 2 == 0), list1))
 
 print("Even numbers in the list: ", even_nos)
